54f54d3229aa973f5dba78fe9c37d4b1bd4e6e22
Shreya Rajput <shreyaaraj1996@gmail.com>
2020-06-01 04:21:26 -0700

    question again added

--- src/Quiz/QuizData.js
+++ src/Quiz/QuizData.js
@@ -10,311 +10,310 @@ const QuizData = [
     question: `Who is the CEO of Tesla Motors?`,
     options: [`Bill Gates`, `Steve Jobs`, `Elon Musk`],
     answer: `Elon Musk`
-    // },
-    // {
-    //   id: 3,
-    //   question: `Name World's Richest Man?`,
-    //   options: [`Jeff Bezo`, `Bill Gates`, `Mark Zuckerberg`],
-    //   answer: `Jeff Bezo`
-    // },
-    // {
-    //   id: 4,
-    //   question: `World's Longest River?`,
-    //   options: [`River Nile`, `River Amazon`, `River Godavari`],
-    //   answer: `River Nile`
-    // },
-    // {
-    //   id: 5,
-    //   question: ` Which is the latitude that runs through the centre of the Earth?`,
-    //   options: [`Tropics of Cancer`, `Capricorn`, `equator`],
-    //   answer: `Equator`
-    // },
-    // {
-    //   id: 6,
-    //   question: `  Which is the tallest mountain in the world?`,
-    //   options: [`K2`, `Mount Everest`, `Kanchenjunga`],
-    //   answer: `Mount Everest`
-    // },
-    // {
-    //   id: 7,
-    //   question: `Which is the biggest oceans of the world?`,
-    //   options: [`Southern`, `Pacific`, `Atlantic`],
-    //   answer: `Pacific`
-    // },
-    // {
-    //   id: 8,
-    //   question: `Which country has the highest population?`,
-    //   options: [`United States`, `China`, `India`],
-    //   answer: `China`
-    // },
-    // {
-    //   id: 9,
-    //   question: ` Which is the largest waterfall in the world?`,
-    //   options: [`Niagara Falls`, `Victoria Falls`, `Iguazu Falls`],
-    //   answer: ` Victoria Falls`
-    // },
-    // {
-    //   id: 10,
-    //   question: `Which is the largest desert in the world?`,
-    //   options: [`Sahara Desert`, `Syrian Desert `, `Great Basin Desert`],
-    //   answer: `Sahara Desert`
-    // },
-    // {
-    //   id: 11,
-    //   question: `Branches of a river are called Tributaries?`,
-    //   options: [`true`, `false`],
-    //   answer: `true`
-    // },
-    // {
-    //   id: 12,
-    //   question: ` Which is the largest island in the world?`,
-    //   options: [`New Guinea`, `Greenland`, `Borneo`],
-    //   answer: `Greenland`
-    // },
-    // {
-    //   id: 13,
-    //   question: `Magma does not heats the water in a hot spring.`,
-    //   options: [`true`, `false`],
-    //   answer: `false`
-    // },
-    // {
-    //   id: 14,
-    //   question: `Which is the coldest continent in the world? `,
-    //   options: [`Antarctica `, `North-America`, `Europe`],
-    //   answer: ` Antarctica`
-    // },
-    // {
-    //   id: 15,
-    //   question: `Which is the world’s smallest country? `,
-    //   options: [` Monaco`, `San Marino`, `Vatican City`],
-    //   answer: `Vatican City `
-    // },
-    // {
-    //   id: 16,
-    //   question: `Where is the Tower Bridge located? `,
-    //   options: [`London,UK`, `NewYork,US`, `Beijing,China`],
-    //   answer: `London, UK `
-    // },
-    // {
-    //   id: 17,
-    //   question: `Where is the Leaning Tower of located? `,
-    //   options: [`London,UK `, `Pisa,Italy`, `Paris,France`],
-    //   answer: ` Pisa, Italy `
-    // },
-    // {
-    //   id: 18,
-    //   question: `How many countries are present in Africa? `,
-    //   options: [`46 countries`, `54 countries`, `43 countries`],
-    //   answer: `54 countries `
-    // },
-    // {
-    //   id: 19,
-    //   question: `Which planet is closest to Planet Earth? `,
-    //   options: [`Venus`, `Mars`, `Juspiter`],
-    //   answer: ` Venus`
-    // },
-    // {
-    //   id: 20,
-    //   question: ` What is the currency in Cambodia?`,
-    //   options: [`Euro `, `Yen`, `Riel `],
-    //   answer: `Riel `
-    // },
-    // {
-    //   id: 21,
-    //   question: `How do you measure the speed of wind?`,
-    //   options: [`Protractor`, `Hygrometer `, `Anemometer`],
-    //   answer: `Anemometer `
-    // },
-    // {
-    //   id: 22,
-    //   question: `Name the river that is not crossed by any bridges. `,
-    //   options: [`Nile`, `Amazon `, `Yangtze`],
-    //   answer: `Amazon `
-    // },
-    // {
-    //   id: 23,
-    //   question: `Name the city that is located in two countries. `,
-    //   options: [`Istanbul`, `Charleroi`, `Damascus`],
-    //   answer: `Istanbul `
-    // },
-    // {
-    //   id: 24,
-    //   question: `Which line separates Indian and China? `,
-    //   options: [`Durand Line`, `McMahon Line`, `Mac-Mahon line `],
-    //   answer: `Mac-Mahon line `
-    // },
-    // {
-    //   id: 25,
-    //   question: `Where is Acropolis situated? `,
-    //   options: [`Athens, Greece `, `Mannila, Philippines`, `Isfahan, Iran `],
-    //   answer: `Athens, Greece `
-    // },
-    // {
-    //   id: 26,
-    //   question: `Which country produces maximum aluminium in the world? `,
-    //   options: [`Philippines `, `Jamaica`, `India`],
-    //   answer: `Jamaica `
-    // },
-    // {
-    //   id: 27,
-    //   question: `Where can you find the ‘Desert of Death’? `,
-    //   options: [`Gobi Desert`, `Dasht-e Margo`, `Sahara Desert`],
-    //   answer: `Dasht-e Margo`
-    // },
-    // {
-    //   id: 28,
-    //   question: `What type of deserts are Gobi desert and Atacama desert? `,
-    //   options: [`Rainshadow deserts`, `Subtropical deserts`, `Coastal deserts`],
-    //   answer: ` Rainshadow deserts`
-    // },
-    // {
-    //   id: 29,
-    //   question: `On which day of the year is the Sun closest to the Earth? `,
-    //   options: [`May 27th `, `January 4th `, `December 22nd`],
-    //   answer: `January 4th `
-    // },
-    // {
-    //   id: 30,
-    //   question: `What is the highest active volcano in Europe? `,
-    //   options: [`Vesuvius`, `Yellowstone`, `Mount Etna `],
-    //   answer: `Mount Etna `
-    // },
-    // {
-    //   id: 31,
-    //   question: `The Buran wind blows across: `,
-    //   options: [`Eastern Asia `, `Eastern Australia`, `Southern Africa`],
-    //   answer: `Eastern Asia `
-    // },
-    // {
-    //   id: 32,
-    //   question: `Which of the following countries does not share its border with Black Sea? `,
-    //   options: [`Georgia`, ` Bulgaria`, `  Belarus`],
-    //   answer: `Belarus `
-    // },
-    // {
-    //   id: 33,
-    //   question: `Which of the following is not an example of systematic approach of geography? `,
-    //   options: [
-    //     `Human geography`,
-    //     `Regional geography `,
-    //     ` Environmental geography `
-    //   ],
-    //   answer: `Regional geography `
-    // },
-    // {
-    //   id: 34,
-    //   question: `Next to sun, which is the nearest star of the earth? `,
-    //   options: [`Apha centuari `, `Usra Major`, `Sirius `],
-    //   answer: `Apha centuari `
-    // },
-    // {
-    //   id: 35,
-    //   question: `Which of the following countries is not larger than India in terms of geographical area?`,
-    //   options: [`Brazil `, `Canada`, `Indonesia`],
-    //   answer: `Indonesia `
-    // },
-    // {
-    //   id: 36,
-    //   question: `Which of the following is an example of direct source to study interior of the earth?  `,
-    //   options: [`Volcanic Eruption `, ` Magnetic field`, ` Sesimic activity `],
-    //   answer: `Volcanic Eruption `
-    // },
-    // {
-    //   id: 37,
-    //   question: `Which of the following rocks are known as Primary rocks? `,
-    //   options: [`Sedimentary Rocks`, `Metamorphic Rocks `, `Igneous Rocks `],
-    //   answer: `Igneous Rocks`
-    // },
-    // {
-    //   id: 38,
-    //   question: `Which among the following country has the largest share in global mangrove areas? `,
-    //   options: [`Brazil`, `Mexico`, `Indonesia `],
-    //   answer: `Indonesia `
-    // },
-    // {
-    //   id: 39,
-    //   question: `Littoral forests are rich sources of which among the following? `,
-    //   options: [`Firewood`, `Timber`, `Bamboo `],
-    //   answer: `Firewood `
-    // },
-    // {
-    //   id: 40,
-    //   question: `Which among the following starts represent the Morning and Evening Stars? `,
-    //   options: [`Mercury, Venus`, `Mercury, Mercury`, `Venus, Venus`],
-    //   answer: `Venus, Venus `
-    // },
-    // {
-    //   id: 41,
-    //   question: `Which of the following is the largest of the seven member states of United Arab Emirates? `,
-    //   options: [`Abu Dhabi `, `Ras al-Khaimah`, `Sharjah`],
-    //   answer: `Abu Dhabi `
-    // },
-    // {
-    //   id: 42,
-    //   question: `Apart from Saturn which of the following possess planetary possess ring systems of their own? `,
-    //   options: [
-    //     `Jupiter, Uranus & Neptune`,
-    //     `Jupiter & Uranus `,
-    //     `Jupiter & Neptune `
-    //   ],
-    //   answer: `Jupiter, Uranus & Neptune `
-    // },
-    // {
-    //   id: 43,
-    //   question: `What is approximate diameter of Milky Way? `,
-    //   options: [
-    //     `100000 light years `,
-    //     `10000 light years `,
-    //     ` 1000000 Light Years`
-    //   ],
-    //   answer: ` 100000 light years`
-    // },
-    // {
-    //   id: 44,
-    //   question: `Chomolungma is located in which country? `,
-    //   options: [`India`, `Bhutan`, `  Nepal`],
-    //   answer: ` Nepal `
-    // },
-    // {
-    //   id: 45,
-    //   question: `On which of the following dates Autumnal Equinox occurs? `,
-    //   options: [`March 21`, ` September 23 `, `June 3`],
-    //   answer: ` September 23 `
-    // },
-    // {
-    //   id: 46,
-    //   question: `In India when there is summer season, what is the season in Chile? `,
-    //   options: [`Summer`, `Winter `, `Autumn `],
-    //   answer: `Winter `
-    // },
-    // {
-    //   id: 47,
-    //   question: `Which among the following is not among the ancient list of seven wonders of world? `,
-    //   options: [`Statue of Zeus at Olympia `, `Great Wall of China`, `Tajmahal`],
-    //   answer: `Great Wall of China `
-    // },
-    // {
-    //   id: 48,
-    //   question: `Nickel & Iron are the main constituents of __________? `,
-    //   options: [`Earth Core `, `Earth Crust`, `Mantle`],
-    //   answer: `Earth Core `
-    // },
-    // {
-    //   id: 49,
-    //   question: `While going towards the centre of the earth, the weight of a body shall ? `,
-    //   options: [`Increase `, `Decrease `, `Remain Constant`],
-    //   answer: `Decrease `
-    // },
-    // {
-    //   id: 50,
-    //   question: `The International Space Station is located in which among the following orbits? `,
-    //   options: [
-    //     `Low Earth Orbit (LEO) `,
-    //     `Medium Earth orbit (MEO) `,
-    //     `Geostationary Orbit (GEO)`
-    //   ],
-    //   answer: `Low Earth Orbit (LEO) `
-    // }
+  },
+  {
+    id: 3,
+    question: `Name World's Richest Man?`,
+    options: [`Jeff Bezo`, `Bill Gates`, `Mark Zuckerberg`],
+    answer: `Jeff Bezo`
+  },
+  {
+    id: 4,
+    question: `World's Longest River?`,
+    options: [`River Nile`, `River Amazon`, `River Godavari`],
+    answer: `River Nile`
+  },
+  {
+    id: 5,
+    question: ` Which is the latitude that runs through the centre of the Earth?`,
+    options: [`Tropics of Cancer`, `Capricorn`, `equator`],
+    answer: `Equator`
+  },
+  {
+    id: 6,
+    question: `  Which is the tallest mountain in the world?`,
+    options: [`K2`, `Mount Everest`, `Kanchenjunga`],
+    answer: `Mount Everest`
+  },
+  {
+    id: 7,
+    question: `Which is the biggest oceans of the world?`,
+    options: [`Southern`, `Pacific`, `Atlantic`],
+    answer: `Pacific`
+  },
+  {
+    id: 8,
+    question: `Which country has the highest population?`,
+    options: [`United States`, `China`, `India`],
+    answer: `China`
+  },
+  {
+    id: 9,
+    question: ` Which is the largest waterfall in the world?`,
+    options: [`Niagara Falls`, `Victoria Falls`, `Iguazu Falls`],
+    answer: ` Victoria Falls`
+  },
+  {
+    id: 10,
+    question: `Which is the largest desert in the world?`,
+    options: [`Sahara Desert`, `Syrian Desert `, `Great Basin Desert`],
+    answer: `Sahara Desert`
+  },
+  {
+    id: 11,
+    question: `Branches of a river are called Tributaries?`,
+    options: [`true`, `false`],
+    answer: `true`
+  },
+  {
+    id: 12,
+    question: ` Which is the largest island in the world?`,
+    options: [`New Guinea`, `Greenland`, `Borneo`],
+    answer: `Greenland`
+  },
+  {
+    id: 13,
+    question: `Magma does not heats the water in a hot spring.`,
+    options: [`true`, `false`],
+    answer: `false`
+  },
+  {
+    id: 14,
+    question: `Which is the coldest continent in the world? `,
+    options: [`Antarctica `, `North-America`, `Europe`],
+    answer: ` Antarctica`
+  },
+  {
+    id: 15,
+    question: `Which is the world’s smallest country? `,
+    options: [` Monaco`, `San Marino`, `Vatican City`],
+    answer: `Vatican City `
+  },
+  {
+    id: 16,
+    question: `Where is the Tower Bridge located? `,
+    options: [`London,UK`, `NewYork,US`, `Beijing,China`],
+    answer: `London, UK `
+  },
+  {
+    id: 17,
+    question: `Where is the Leaning Tower of located? `,
+    options: [`London,UK `, `Pisa,Italy`, `Paris,France`],
+    answer: ` Pisa, Italy `
+  },
+  {
+    id: 18,
+    question: `How many countries are present in Africa? `,
+    options: [`46 countries`, `54 countries`, `43 countries`],
+    answer: `54 countries `
+  },
+  {
+    id: 19,
+    question: `Which planet is closest to Planet Earth? `,
+    options: [`Venus`, `Mars`, `Juspiter`],
+    answer: ` Venus`
+  },
+  {
+    id: 20,
+    question: ` What is the currency in Cambodia?`,
+    options: [`Euro `, `Yen`, `Riel `],
+    answer: `Riel `
+  },
+  {
+    id: 21,
+    question: `How do you measure the speed of wind?`,
+    options: [`Protractor`, `Hygrometer `, `Anemometer`],
+    answer: `Anemometer `
+  },
+  {
+    id: 22,
+    question: `Name the river that is not crossed by any bridges. `,
+    options: [`Nile`, `Amazon `, `Yangtze`],
+    answer: `Amazon `
+  },
+  {
+    id: 23,
+    question: `Name the city that is located in two countries. `,
+    options: [`Istanbul`, `Charleroi`, `Damascus`],
+    answer: `Istanbul `
+  },
+  {
+    id: 24,
+    question: `Which line separates Indian and China? `,
+    options: [`Durand Line`, `McMahon Line`, `Mac-Mahon line `],
+    answer: `Mac-Mahon line `
+  },
+  {
+    id: 25,
+    question: `Where is Acropolis situated? `,
+    options: [`Athens, Greece `, `Mannila, Philippines`, `Isfahan, Iran `],
+    answer: `Athens, Greece `
+  },
+  {
+    id: 26,
+    question: `Which country produces maximum aluminium in the world? `,
+    options: [`Philippines `, `Jamaica`, `India`],
+    answer: `Jamaica `
+  },
+  {
+    id: 27,
+    question: `Where can you find the ‘Desert of Death’? `,
+    options: [`Gobi Desert`, `Dasht-e Margo`, `Sahara Desert`],
+    answer: `Dasht-e Margo`
+  },
+  {
+    id: 28,
+    question: `What type of deserts are Gobi desert and Atacama desert? `,
+    options: [`Rainshadow deserts`, `Subtropical deserts`, `Coastal deserts`],
+    answer: ` Rainshadow deserts`
+  },
+  {
+    id: 29,
+    question: `On which day of the year is the Sun closest to the Earth? `,
+    options: [`May 27th `, `January 4th `, `December 22nd`],
+    answer: `January 4th `
+  },
+  {
+    id: 30,
+    question: `What is the highest active volcano in Europe? `,
+    options: [`Vesuvius`, `Yellowstone`, `Mount Etna `],
+    answer: `Mount Etna `
+  },
+  {
+    id: 31,
+    question: `The Buran wind blows across: `,
+    options: [`Eastern Asia `, `Eastern Australia`, `Southern Africa`],
+    answer: `Eastern Asia `
+  },
+  {
+    id: 32,
+    question: `Which of the following countries does not share its border with Black Sea? `,
+    options: [`Georgia`, ` Bulgaria`, `  Belarus`],
+    answer: `Belarus `
+  },
+  {
+    id: 33,
+    question: `Which of the following is not an example of systematic approach of geography? `,
+    options: [
+      `Human geography`,
+      `Regional geography `,
+      ` Environmental geography `
+    ],
+    answer: `Regional geography `
+  },
+  {
+    id: 34,
+    question: `Next to sun, which is the nearest star of the earth? `,
+    options: [`Apha centuari `, `Usra Major`, `Sirius `],
+    answer: `Apha centuari `
+  },
+  {
+    id: 35,
+    question: `Which of the following countries is not larger than India in terms of geographical area?`,
+    options: [`Brazil `, `Canada`, `Indonesia`],
+    answer: `Indonesia `
+  },
+  {
+    id: 36,
+    question: `Which of the following is an example of direct source to study interior of the earth?  `,
+    options: [`Volcanic Eruption `, ` Magnetic field`, ` Sesimic activity `],
+    answer: `Volcanic Eruption `
+  },
+  {
+    id: 37,
+    question: `Which of the following rocks are known as Primary rocks? `,
+    options: [`Sedimentary Rocks`, `Metamorphic Rocks `, `Igneous Rocks `],
+    answer: `Igneous Rocks`
+  },
+  {
+    id: 38,
+    question: `Which among the following country has the largest share in global mangrove areas? `,
+    options: [`Brazil`, `Mexico`, `Indonesia `],
+    answer: `Indonesia `
+  },
+  {
+    id: 39,
+    question: `Littoral forests are rich sources of which among the following? `,
+    options: [`Firewood`, `Timber`, `Bamboo `],
+    answer: `Firewood `
+  },
+  {
+    id: 40,
+    question: `Which among the following starts represent the Morning and Evening Stars? `,
+    options: [`Mercury, Venus`, `Mercury, Mercury`, `Venus, Venus`],
+    answer: `Venus, Venus `
+  },
+  {
+    id: 41,
+    question: `Which of the following is the largest of the seven member states of United Arab Emirates? `,
+    options: [`Abu Dhabi `, `Ras al-Khaimah`, `Sharjah`],
+    answer: `Abu Dhabi `
+  },
+  {
+    id: 42,
+    question: `Apart from Saturn which of the following possess planetary possess ring systems of their own? `,
+    options: [
+      `Jupiter, Uranus & Neptune`,
+      `Jupiter & Uranus `,
+      `Jupiter & Neptune `
+    ],
+    answer: `Jupiter, Uranus & Neptune `
+  },
+  {
+    id: 43,
+    question: `What is approximate diameter of Milky Way? `,
+    options: [
+      `100000 light years `,
+      `10000 light years `,
+      ` 1000000 Light Years`
+    ],
+    answer: ` 100000 light years`
+  },
+  {
+    id: 44,
+    question: `Chomolungma is located in which country? `,
+    options: [`India`, `Bhutan`, `  Nepal`],
+    answer: ` Nepal `
+  },
+  {
+    id: 45,
+    question: `On which of the following dates Autumnal Equinox occurs? `,
+    options: [`March 21`, ` September 23 `, `June 3`],
+    answer: ` September 23 `
+  },
+  {
+    id: 46,
+    question: `In India when there is summer season, what is the season in Chile? `,
+    options: [`Summer`, `Winter `, `Autumn `],
+    answer: `Winter `
+  },
+  {
+    id: 47,
+    question: `Which among the following is not among the ancient list of seven wonders of world? `,
+    options: [`Statue of Zeus at Olympia `, `Great Wall of China`, `Tajmahal`],
+    answer: `Great Wall of China `
+  },
+  {
+    id: 48,
+    question: `Nickel & Iron are the main constituents of __________? `,
+    options: [`Earth Core `, `Earth Crust`, `Mantle`],
+    answer: `Earth Core `
+  },
+  {
+    id: 49,
+    question: `While going towards the centre of the earth, the weight of a body shall ? `,
+    options: [`Increase `, `Decrease `, `Remain Constant`],
+    answer: `Decrease `
+  },
+  {
+    id: 50,
+    question: `The International Space Station is located in which among the following orbits? `,
+    options: [
+      `Low Earth Orbit (LEO) `,
+      `Medium Earth orbit (MEO) `,
+      `Geostationary Orbit (GEO)`
+    ],
+    answer: `Low Earth Orbit (LEO) `
   }
 ];
 
